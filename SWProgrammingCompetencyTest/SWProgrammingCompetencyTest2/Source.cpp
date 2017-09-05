e591cfbc6c9aa35304b37c1d73d123ae0a7741b7
TUAN ANH NGUYEN/LGEVH VC SOFTWARE DEVELOPMENT 2(tuan3.nguyen@lge.com) <tuan3.nguyen@lge.com>
2017-09-05 15:52:47 +0700

    [Geely][NL5][GP-HMI] Fix bug with test 2
    [DEV]
    [Desc][0]
    R/C :
    C/A : Fix bug with test 2

--- SWProgrammingCompetencyTest/SWProgrammingCompetencyTest2/Source.cpp
+++ SWProgrammingCompetencyTest/SWProgrammingCompetencyTest2/Source.cpp
@@ -7,57 +7,58 @@ int main()
 {
     int N = 0;
     cin >> N;
-
-    int a[N][N];	// Matrix to store the fine dust change of each day (row) and each hour (column)
+    int *a = new int[N*N];
 
     // Section to input the fine dust change for each of N hours and N days
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            cin >> a[i][j];
+            cin >> a[i*N + j];
         }
     }
 
     int max_row = 0;	// Variable to store the day (row) of the largest sum of fine dust changes
     int min_col = 0;	// Variable to store the hour (column) of the smallest sum of fine dust changes
 
-    int sum = 0;		// Variable to store the sum of fine dust changes
+    long long sum = 0;		// Variable to store the sum of fine dust changes
 
-    int min_sum = 0;	// Variable to store the smallest sum of fine dust changes
+    long long min_sum = 0;
 
 
     // Section to calculate which time slot (column) has the smallest sum of fine dust changes
     for (int j = 0; j < N; j++) {
-
+        sum = 0;
         // Calculate the sum of fine dust changes of jth hour (column) during N days
         for (int i = 0; i < N; i++) {
-            sum += a[i][j];
+            sum += a[i*N + j];
         }
 
         // 각 시간대(열)의 미세먼지 변화량 합이 가장 작은 시간(열)을 구하는 부분
-        if (sum < min_sum){
+        if (sum < min_sum || j == 0){
             min_sum = sum;
             min_col = j;
         }
     }
 
-    int max_sum = 0;	// Variable to store the largest sum of fine dust changes
+    long long max_sum = 0;
 
     // Section to calculate which day (row) has the largest sum of fine dust changes
     for (int i = 0; i < N; i++)	{
+        sum = 0;
 
         // Calculate the sum of fine dust changes for N hours on the ith day (row)
         for (int j = 0; j < N; j++) {
-            sum += a[i][j];
+            sum += a[i*N + j];
         }
 
         // Section to obtain the day (row) that has the largest sum of fine dust changes on each day (row)
-        if (sum > max_sum) {
+        if (sum > max_sum || i == 0) {
             max_sum = sum;
             max_row = i;
         }
     }
 
     cout << (max_row + 1) << " " << (min_col + 1) << endl;
-
+    delete[]a;
+    system("PAUSE");
     return 0;
 }
