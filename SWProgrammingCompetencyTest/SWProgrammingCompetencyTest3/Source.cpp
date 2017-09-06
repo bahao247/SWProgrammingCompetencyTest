eec363eeb953be16f77044d66903d0ebbbc7f68a
TUAN ANH NGUYEN/LGEVH VC SOFTWARE DEVELOPMENT 2(tuan3.nguyen@lge.com) <tuan3.nguyen@lge.com>
2017-09-06 14:04:41 +0700

    [Bahao247] Fix code project test 3
    [DEV]
    [Desc][0]
    R/C :
    C/A : Fix code project test 3

--- SWProgrammingCompetencyTest/SWProgrammingCompetencyTest3/Source.cpp
+++ SWProgrammingCompetencyTest/SWProgrammingCompetencyTest3/Source.cpp
@@ -9,6 +9,7 @@ typedef struct{
 
 int     N; // Number of projects
 DATA    project_info[MAX_PROJECT]; // Project Infomation
+int mMaxProject = 0;
 
 void Data_Input(void) {
     cin >> N; // Input of number of projects
@@ -18,10 +19,43 @@ void Data_Input(void) {
     }
 }
 
+void ResearchProject()
+{
+    int mCountProject = 1;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+        {
+            //project_info[i].start_date >> project_info[i].end_date;
+            if (i != j)
+            {
+                if (project_info[i].start_date > project_info[j].end_date)
+                {
+                    ++mCountProject;
+                }
+                else
+                {
+                    if (project_info[i].end_date < project_info[j].start_date)
+                    {
+                        ++mCountProject;
+                    }
+                }
+            }
+        }
+        if (mCountProject > mMaxProject)
+        {
+            mMaxProject = mCountProject;
+        }
+        mCountProject = 1;
+    }
+    
+}
+
 int main(void) {
     int sol = 0;
     Data_Input(); // Calling of input function
-
+    ResearchProject();
+    sol = mMaxProject;
     cout << sol << endl; // Output of result
+    system("PAUSE");
     return 0;
 }
