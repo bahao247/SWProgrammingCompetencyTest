c19e18eee888c0afc7084727e8bb3ce1769160f7
TUAN ANH NGUYEN/LGEVH VC SOFTWARE DEVELOPMENT 2(tuan3.nguyen@lge.com) <tuan3.nguyen@lge.com>
2017-09-18 11:17:42 +0700

    [Bahao247] Implement Drill Machine Function for Smart Factory
    [DEV]
    [Desc][0]
    R/C :
    C/A : Implement Drill Machine Function for Smart Factory

--- SWProgrammingCompetencyTest/SWProgrammingCompetencyTest4/Source.cpp
+++ SWProgrammingCompetencyTest/SWProgrammingCompetencyTest4/Source.cpp
@@ -1,22 +1,88 @@
-#include <iostream>
+#include <stdio.h>
 using namespace std;
-int H;//Height
-int W;//Width
-char map[500][510];//map
+#define MAXQ (500*500+10)
+int mHeight;//Height
+int mWeight;//Width
+char map[500][510];//Map
+int visit[500][500];
 
-void input_data(){
+void input_data(void){
     int i;
-    cin >> H >> W;
-    for (i = 0; i < H; i++){
-        cin >> map[i];
+    scanf_s("%d %d", &mHeight, &mWeight);
+    for (i = 0; i < mHeight; i++){
+        scanf_s("%s", map[i]);
     }
 }
 
-int main(){
+struct sSaveStatus{
+    int height, weight, temp;
+};
+
+int wpTempIndex, rpNowIndex;
+int goUpDown[] = {-1, 1, 0, 0};
+int goRightLeft[] = {0, 0, -1, 1};
+sSaveStatus queue[MAXQ];
+
+int FindMinDistance()
+{
+    int i, height, weight;
+    sSaveStatus data;
+
+    wpTempIndex = rpNowIndex = 0;
+
+    queue[wpTempIndex].height = 0;
+    queue[wpTempIndex].weight = 0;
+    queue[wpTempIndex++].temp = 0;
+
+    visit[0][0] = 1;
+
+    while (rpNowIndex < wpTempIndex)
+    {
+        data = queue[rpNowIndex++];
+        for (i = 0; i < 4; i++)
+        {
+            height = data.height + goUpDown[i];
+            weight = data.weight + goRightLeft[i];
+
+            //Check condition
+            if ((height < 0) || (height >= mHeight) || (weight < 0) || (weight >= mWeight))
+            {
+                continue;
+            }
+
+            //Check tree
+            if (map[height][weight] == 'X')
+            {
+                continue;
+            }
+
+            //Check old street 
+            if (visit[height][weight] == 1)
+            {
+                continue;
+            }
+
+            if ((height == mHeight - 1) && (weight == mWeight - 1))
+            {
+                return data.temp + 1;
+            }
+            queue[wpTempIndex].height = height;
+            queue[wpTempIndex].weight = weight;
+            queue[wpTempIndex++].temp = data.temp + 1;
+            visit[height][weight] = 1;
+        }
+    }
+
+    return -1;
+}
+
+int main(void){
     int ans = 0;
     input_data();
-    // Todo : write the code
 
-    cout << ans << endl;
+    // Todo : write the code
+    ans = FindMinDistance();
+
+    printf("%d\n", ans);
     return 0;
 }
